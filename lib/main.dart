58cd677743944a49c133d48552940a56d22c6356
Denis Savishchev <denis.savishchev@gmail.com>
2023-04-28 21:19:32 +0200

    provider_json

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:test_file/state_management/state_management.dart';
 import 'package:test_file/widgets_lesons_two.dart';
 import 'package:test_file/widgets_lessons.dart';
 // import 'package:test_file/staggered_animation.dart';
-// import 'package:test_file/text_field.dart';
+
 import 'adaptive_responsive_layout_widget.dart';
 import 'orange_list.dart';
 // import 'auth.dart';
